76290554968e1d2a40bcec22d62479574b2b14fa
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-05 14:03:48 +0200

    Update logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,33 +6,23 @@ export default function Navbar() {
       <nav className="flex justify-between items-center mb-6">
         <NavLink to="/">
           <img
-            alt="MongoDB logo"
-            className="h-10 inline"
-            src="https://raw.githubusercontent.com/mongodb-developer/mern-stack-example/603144e25ba5549159d1962601337652a7bfa253/mern/client/src/assets/mongodb.svg"
+            alt="Care Chemnitz logo"
+            className="h-20 inline"
+            src="../src/assets/Chemnitz_care_text.svg"
           ></img>
         </NavLink>
-
-        <NavLink
-          className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
-          to="/create"
-        >
-          Create Employee
-        </NavLink>
-
         <NavLink
           className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
           to="/profile"
         >
           Profile
         </NavLink>
-
         <NavLink
           className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
           to="/logout"
         >
           Log Out
         </NavLink>
-
         <NavLink
           className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
           to="/signin"
