951d2fffd5b2d15e7dae7477c26818a09b398827
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-06 13:33:44 +0200

    Put design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,9 @@ const App = () => {
   return (
     <div className="min-h-full">
       <Navbar />
-      <Outlet />
+      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <Outlet />
+      </div>
     </div>
   );
 };
