46c96673044e56fbe0b0533680d9a9044e213c9b
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-07 17:54:38 +0200

    Changed Outlet UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,7 @@ const App = () => {
   return (
     <div className="min-h-full">
       <Navbar />
-      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   );
 };
