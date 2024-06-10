0d2f4bf9936283d919f9e9079caaf15c82d48b55
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-10 12:42:48 +0200

    Added Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="min-h-full">
+    <div className="min-h-svh">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
