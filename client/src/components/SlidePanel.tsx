f51815f66ca5226be226a93ce50305eae14ad2b0
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-10 12:43:19 +0200

    Added exception

--- client/src/components/SlidePanel.tsx
+++ client/src/components/SlidePanel.tsx
@@ -85,8 +85,20 @@ export default function SlidePanel({ geojsonData, loading }: SlidePanelProps) {
   };
 
   return (
-    <ul role="list" className="divide-y divide-gray-100">
-      {facilityList()}
+    <ul role="list" className="divide-y divide-gray-100 overflow-y-auto h-dvh">
+      {loading ? (
+        <li className="flex justify-between gap-x-6 py-5">
+          <div className="flex min-w-0 gap-x-4">
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                Loading...
+              </p>
+            </div>
+          </div>
+        </li>
+      ) : (
+        facilityList()
+      )}
     </ul>
   );
 }
