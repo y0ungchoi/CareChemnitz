8361d7ed5561e7814120092dfe0f36617604b9de
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-10 16:56:03 +0200

    Put more data

--- client/src/components/SlidePanel.tsx
+++ client/src/components/SlidePanel.tsx
@@ -4,34 +4,27 @@ import { GeojsonResponse, GeojsonFeature } from "./Maps";
 function Facility({
   feature,
   name,
-  isOpenDetail,
+  handleFacilityClick,
 }: {
   feature: GeojsonFeature;
   name: string;
-  isOpenDetail: () => void;
+  handleFacilityClick: () => void;
 }) {
   let facilityName = feature.properties.TRAEGER;
   let address = `${feature.properties.STRASSE}, ${feature.properties.PLZ} ${feature.properties.ORT}`;
-  let contact = feature.properties.TELEFON;
-  let website = "";
-  let creator = "GISAdminChemnitz";
 
   if (name === "Schulen") {
     facilityName = feature.properties.BEZEICHNUNG;
-    contact = `${contact}, ${feature.properties.EMAIL}`;
-    website = feature.properties.WWW;
   } else if (name === "Kindertageseinrichtungen") {
     let old = feature.properties.BEZEICHNUNG;
     facilityName = old.substring(old.indexOf('"') + 1, old.lastIndexOf('"'));
     address = `${feature.properties.STRASSE} ${feature.properties.HAUSBEZ}, ${feature.properties.PLZ} ${feature.properties.ORT}`;
-    contact = `${feature.properties.TELEFON}, ${feature.properties.EMAIL}`;
-    website = feature.properties.URL;
   }
 
   return (
     <li className="flex justify-between gap-x-6 py-5 hover:bg-gray-200">
       <div className="flex min-w-0 gap-x-4">
-        <a className="min-w-0 flex-auto" onClick={isOpenDetail}>
+        <a className="min-w-0 flex-auto" onClick={handleFacilityClick}>
           <p className="text-sm font-semibold leading-6 text-gray-900">
             {facilityName}
           </p>
@@ -90,7 +83,7 @@ export default function SlidePanel({ geojsonData, loading }: SlidePanelProps) {
           feature={feature}
           name={geojson.name}
           key={`${index}-${featureIndex}`}
-          isOpenDetail={() => handleFacilityClick(geojson.name, feature)}
+          handleFacilityClick={() => handleFacilityClick(geojson.name, feature)}
         />
       ))
     );
@@ -110,23 +103,25 @@ export default function SlidePanel({ geojsonData, loading }: SlidePanelProps) {
       WWW,
       URL,
       HAUSBEZ,
+      Creator,
     } = selectedFacility.feature.properties;
 
     let facilityName = TRAEGER || "";
     let address = `${STRASSE}, ${PLZ} ${ORT}`;
     let contact = TELEFON;
     let website = WWW || URL || "";
+    let creator = Creator;
+    let lat = selectedFacility.feature.geometry.coordinates[0];
+    let lng = selectedFacility.feature.geometry.coordinates[1];
 
     if (selectedFacility.name === "Schulen") {
       facilityName = BEZEICHNUNG;
       contact = `${contact}, ${EMAIL}`;
-      // website = feature.properties.WWW;
     } else if (selectedFacility.name === "Kindertageseinrichtungen") {
       let old = BEZEICHNUNG;
       facilityName = old.substring(old.indexOf('"') + 1, old.lastIndexOf('"'));
       address = `${STRASSE} ${HAUSBEZ}, ${PLZ} ${ORT}`;
       contact = `${TELEFON}, ${EMAIL}`;
-      // website = feature.properties.URL;
     }
 
     return (
@@ -137,6 +132,10 @@ export default function SlidePanel({ geojsonData, loading }: SlidePanelProps) {
         <h2 className="text-xl font-bold">{facilityName}</h2>
         <p>{address}</p>
         <p>{contact}</p>
+        <p>{creator}</p>
+        <p>{lat}</p>
+        <p>{lng}</p>
+
         {website && (
           <p>
             <a
