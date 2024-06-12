8c1785315b3a18accbc94f17c08cf54bd61b8415
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-12 16:20:06 +0200

    Removed console log

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -34,7 +34,6 @@ export default function Signin() {
             )
           : null;
       }
-      console.log(data.homeLocation);
       navigate("/");
     } else {
       alert("Invalid credentials");
