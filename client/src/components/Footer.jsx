e6ec93c3fd347664582566349ccab16f426a9b6a
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-18 19:21:22 +0200

    Changed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,22 +4,28 @@ function Footer() {
   return (
     <>
       <footer className="bg-background hidden sm:block">
-        <div className="container mx-auto py-[2rem]">
+        <div className="container mx-auto px-[1rem] py-[1rem]">
           {/* footer div all */}
-          <div className="flex justify-between md:flex-row items-center md:items-start md:gap-[5rem]">
+          <div className="flex justify-between flex-col md:flex-row items-center md:items-start md:gap-[2rem] text-left">
             {/* logo side */}
-            <div className="flex md:p-0 py-1 gap-1">
+            <div className="flex flex-col w-1/2 md:p-0 gap-1">
               <img
                 src="../src/assets/Chemnitz_care_text.svg"
                 alt="footer_logo"
                 className="w-[7rem]"
               />
-            </div>
-            <div className="flex md:p-0 py-1 gap-1">
-              <p className="text-[15px] font-medium text-[#646464]">
-                Take your health and body to the next level with.
+              <p className="text-[10px] font-medium text-[#646464]">
+                Children, adolescents, and young adults require access to the
+                education and care system.
               </p>
-              <p className="text-[16px] font-medium text-[#646464] text-right">
+              <p className="text-[10px] font-medium text-[#646464]">
+                This is essential for their development and also legally
+                mandated.
+              </p>
+            </div>
+            <div className="flex flex-col md:p-0 gap-1 relative">
+              <div className="flex flex-col h-[7rem] md:p-0 gap-1"></div>
+              <p className="text-[11px] font-medium text-[#646464] text-right">
                 Privacy Policy | © {new Date().getFullYear()} Care Chemnitz{" "}
                 <br /> Developed by{" "}
                 <a
