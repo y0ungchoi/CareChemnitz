a2e33b24413301aa11f736ea1c4d436533255d6c
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-19 16:15:30 +0200

    Removed unsed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Mainpage from "./pages/Mainpage";
 import Signin from "./pages/Signin";
