d40b502f1fb8a0fad4247e80138184a297c0e353
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-06-25 07:05:22 +0200

    Change the z-index

--- client/src/components/SearchPlace.tsx
+++ client/src/components/SearchPlace.tsx
@@ -69,7 +69,7 @@ function SearchPlace({ onPlaceSelect, item, id }: SearchPlaceProps) {
             </Combobox.Button>
           </div>
 
-          <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+          <Combobox.Options className="absolute z-40 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
             {autoCompletePlaces.length === 0 ? (
               <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                 Nothing found.
